f9ee4ac58c57bb467b595976cf916ab3e67e3c4b
Joel Angel <joel.angel@gitfitcode.com>
2024-10-10 20:41:45 +0300

    Topbar updates

--- app/components/Dashboard/TopBar.tsx
+++ app/components/Dashboard/TopBar.tsx
@@ -8,13 +8,13 @@ export const TopBar = () => {
         <div>
           <span className="text-sm font-bold block">🚀 Good morning, Tom!</span>
           <span className="text-xs block text-stone-500">
-            Tuesday, Aug 8th 2023
+            Thursday, Oct 25th 2024
           </span>
         </div>
 
         <button className="flex text-sm items-center gap-2 bg-stone-100 transition-colors hover:bg-violet-100 hover:text-violet-700 px-3 py-1.5 rounded">
           <FiCalendar />
-          <span>Prev 6 Months</span>
+          <span>Next 6 Months</span>
         </button>
       </div>
     </div>
