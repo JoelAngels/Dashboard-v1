e9586b0e82172fd98f2980054be440e64d02b9e0
Joel Angel <joel.angel@gitfitcode.com>
2024-10-10 20:28:47 +0300

    sidebar done

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Dashboard from "./components/Dashboard/Dashboard";
-import Sidebar from "./components/sidebar/Sidebar";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 export default function Home() {
   return (
